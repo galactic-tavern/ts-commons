ef99cadb607401ca0f2b518762d0f8d76f9edc3f
rene van der ark <rene.vanderark@kb.nl>
2021-01-31 22:36:11 +0100

    adds player interface to commons.

--- common-interfaces/gameplay.ts
+++ common-interfaces/gameplay.ts
@@ -1,12 +1,43 @@
-import { MapItem } from "./game-map";
+import { MapData, MapItem } from "./game-map";
+
+export interface Game {
+    mapId : number
+    gameId : string
+    players : Array<Player>
+    mapData : MapData
+}
 
 export interface DrawLayer {
     layer : string
     mapItems : Array<MapItem>
 }
 
-export interface ViewPortData {
-    layers : Array<DrawLayer>
-    xPos : number
+export enum Direction {UP, RIGHT, LEFT, DOWN};
+export enum PlayerState {STANDING, WALKING, ENDING_WALK}
+
+
+export interface Player {
+    id : string
+    direction : Direction
+    state : PlayerState
+    xPos : number 
     yPos : number
+    lastX : number
+    lastY : number
+    speed : number
+    animIdx : number
+    lastAnimIdx : number
 }
+
+export const initPlayer : (id? : string) => Player = (id) => ({
+    id: id || null,
+    direction: Direction.DOWN,
+    state: PlayerState.STANDING,
+    xPos: 0,
+    yPos: 0,
+    lastX: 0,
+    lastY: 0,
+    speed: 0.13,
+    animIdx: 0,
+    lastAnimIdx: 0
+});
